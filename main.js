280d11b82fa20c7db1955f4b649839d2da671597
sish <100435804+sishxd@users.noreply.github.com>
2022-05-08 10:11:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ app.use(bodyParser.json()); // Body Parser
 app.use(expressip().getIpInfoMiddleware); // IP Information Middleware
 app.use(cookieParser()); // Cookie Parser
 
+mongoose.connect("MONGODB URI", () => { console.log('[DB] | Database: Connected') });
+const schema = require('./database/data.js');
+
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
